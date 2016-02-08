427eedc9ab789f91d6fb2ed3a0259452c13b1948
baitu <zbaitu@gmail.com>
2016-02-08 23:31:59 +0800

    macro format later

--- tests/src/items/macro.rs
+++ tests/src/items/macro.rs
@@ -1,3 +1,34 @@
-macro_rules! f {
-    () => ()
+//
+// macro_rules! a {
+// () => ()
+// }
+//
+
+//
+// macro_rules! fmt_src {
+// ($src:tt, $($value:tt)*) => (format!(src!($src), $($value)*));
+// }
+//
+
+//
+// macro_rules! fmt_src {
+// ($src:tt, $($value:tt)*) => ("a")
+// }
+//
+
+//
+// macro_rules! src {
+// (HEADER) => (r#"
+// pub const HEADER: Header = Header {{
+// name: b"{}",
+// major: {},
+// minor: {},
+// revision: {},
+// }};
+// "#);
+// }
+//
+
+fn f() -> bool {
+    a!{};
 }
