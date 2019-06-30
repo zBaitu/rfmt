92c6d54e9013c1cd1aa5f8e2e59bb2db9708f9c0
baitu <zbaitu@gmail.com>
2019-07-01 00:54:11 +0800

    [save]

--- tests/items/generics.rs
+++ tests/items/generics.rs
@@ -1,20 +1,17 @@
+//type a<> = bool;
 //type a<'a> = bool;
+//type a<T> = bool;
 /*
-type a<'a, 
-'b: 'a, 
-'c: 'a + 'b> = bool;
-*/
+type a<'a,
+    'b: 'a,
+    'c: 'a + 'b> = bool;
+    */
 //type a<T> = bool;
 //type a<'a, T: 'a> = bool;
 //type a<T: Sized> = bool;
 //type a<T: ?Sized> = bool;
 
-/*
-type a<T: for<'a> ::iter<bool>::Iterator<A, 
-    B=A> 
-    + Sized> = bool;
-*/
+type a<T: for<'a> ::iter<bool>::Iterator<A, B=A> + Sized> = bool;
 //type a<T: Fn(A, B) -> ()> = bool;
-//type a<T=u8> = bool;
+//type a<T = u8> = bool;
 
-type a<'a, 'b, T, U> where for<'a> T: Iterator + 'a, U: Option, 'a: 'b = bool;
